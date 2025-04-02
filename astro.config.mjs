894a5ba45779dc7feaff2dbaff31fd02f92d9c63
M. Afifudin <afif.x.udin@gmail.com>
2025-04-03 06:32:42 +0700

    fix: canonical url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  // site: 'http://localhost:4321',
   site: 'https://afiiif.github.io',
   integrations: [tailwind()],
 });
