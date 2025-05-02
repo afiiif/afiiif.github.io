6a735c0d0f7cc23c424e3c37778b48e4601007a9
M. Afifudin <afif.x.udin@gmail.com>
2025-05-02 22:07:39 +0700

    feat: use pixel font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pixel:
+          '"Micro 5", ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      },
+    },
   },
   plugins: [],
 };
