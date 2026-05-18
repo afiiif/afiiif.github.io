3458cd490abf73c6f8558585a459b377cea88f99
M. Afifudin <afif.x.udin@gmail.com>
2026-05-18 14:46:24 +0700

    re-init - astro 6.3 with tailwind 4.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'http://localhost:4321',
-  site: 'https://afiiif.github.io',
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
